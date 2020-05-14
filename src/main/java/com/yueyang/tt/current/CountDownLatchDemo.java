601649a7afbde01517c0db2b92bfbd28a57c6130
qinxiangyang <qinxiangyang@xinhuamm.net>
2020-05-14 17:41:31 +0800

    countDownLatch重写

--- src/main/java/com/yueyang/tt/current/CountDownLatchDemo.java
+++ src/main/java/com/yueyang/tt/current/CountDownLatchDemo.java
@@ -1,16 +1,22 @@
 package com.yueyang.tt.current;
 
-import java.util.concurrent.CountDownLatch;
-
 /**
  * @program: IdleStudy
- * @description:
+ * @description: CountDownLatch 的作用是：当一个线程需要另外一个或多个线程完成后，再开始执行。比如主线程要等待一个子线程完成环境相关配置的加载工作\
+ * ，主线程才继续执行，就可以利用 CountDownLatch 来实现。
+ *
+ *
+ * 首先是创建实例 CountDownLatch countDown = new CountDownLatch(2)
+ * 需要同步的线程执行完之后，计数-1； countDown.countDown()
+ * 需要等待其他线程执行完毕之后，再运行的线程，调用 countDown.await()实现阻塞同步
+ *
+ *
  * @author: qinxiangyang
  * @create: 2020-05-14 16:40
  **/
 public class CountDownLatchDemo {
     public static void main(String[] args) throws
-             Exception{
+            Exception {
 
 
         CountDownLatchRe countDownLatch = new CountDownLatchRe(2);
@@ -23,7 +29,6 @@ public class CountDownLatchDemo {
                 System.out.println(Thread.currentThread().getName() + ",子线程结束执行...");
             }
         }).start();
-
         new Thread(new Runnable() {
 
             @Override
@@ -33,6 +38,7 @@ public class CountDownLatchDemo {
                 System.out.println(Thread.currentThread().getName() + ",子线程结束执行...");
             }
         }).start();
+
         countDownLatch.await();// 減去为0,恢复任务继续执行
         System.out.println("两个子线程执行完毕....");
         System.out.println("主线程继续执行.....");
